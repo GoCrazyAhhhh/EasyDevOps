a32a53c94ac59d6df991de7007c762fc6bf37273
Ruben tax <566465@student.saxion.nl>
2025-12-04 15:07:59 +0100

    foto verplaatst

--- frontend/MyConsoleApp/MyConsoleApp/obj/Debug/net10.0/MyConsoleApp.AssemblyInfo.cs
+++ frontend/MyConsoleApp/MyConsoleApp/obj/Debug/net10.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbc7ed472e08a8fca2dc65bf751308c6f38ac275")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2440a42887c1d1e4c2ac14108524d76e7e482c3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
