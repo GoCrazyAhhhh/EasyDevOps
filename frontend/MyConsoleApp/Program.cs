dbd46646425a5fc141856886edd4db136d41315e
Ruben tax <566465@student.saxion.nl>
2025-12-04 16:26:07 +0100

    Website met plaatje

--- frontend/MyConsoleApp/Program.cs
+++ frontend/MyConsoleApp/Program.cs
@@ -1,7 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// Serve static files from wwwroot
+app.UseStaticFiles();
 
-app.MapGet("/", () => "EasyDevOps");
+app.MapGet("/", () =>
+{
+    return Results.Content(
+        "<html><body><p>EasyDevOps</p> <img src=\"/ITM.jpg\" alt=\"ITM\"></body></html>",
+        "text/html"
+    );
+});
 
 app.Run();
